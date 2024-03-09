71fafc9e1fd8a8343573eae15761d546c9db39b1
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-03-09 21:16:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Загрузка файлов</title>
+</head>
+<body>
+	<h1>Загрузка файлов</h1><br>
+	<hr>
+	
+	 <a href='https://danon-dev.ru/smtest/files.zip' download=""><button>Скачать FILES</button></a>
+	<a href='https://danon-dev.ru/smtest/client.apk' download=""><button>Скачать APK</button></a>
+</body>
+</html>
